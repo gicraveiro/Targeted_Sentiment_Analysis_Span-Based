fixed dum calculation error on attention mask matrix

main.py
@@ -58,31 +58,48 @@ while len(dynamic_dataframe) != 0:
 #print(dynamic_dataframe)
 #print(random_batches_list)
 
+# PADDING AND ATTENTION MASK WITH SMART BATCHING
 
-# PAD AND ATTENTION MASK
-
-padded = []
+#padded_tok_dataset = []
 attention_mask = []
+#pytorch_input_ids = []
+input_ids = []
+#pytorch_attention_mask = []
 
 for batch in random_batches_list:
   max_len = 0
   for sentence in batch:
+    padded_batch = []
+    batch_attention_mask = []
+
     if (len(sentence) > max_len):
       max_len = len(sentence)
       #print(max_len)
   
   for sentence in batch:
     num_zeros = max_len - len(sentence)
-    sentence = sentence + [0] * num_zeros
+ #   print(num_zeros, len(sentence), len(sentence) +num_zeros)
+    
     #print(sentence)
     sentence_attention_mask = (len(sentence)*[1] + num_zeros*[0])
+    sentence = sentence + [0] * num_zeros
     #print(sentence_attention_mask)
+    padded_batch.append(sentence)
+    batch_attention_mask.append(sentence_attention_mask)
 
-  padded.append(sentence)
-  attention_mask.append(sentence_attention_mask)
+  #padded_tok_dataset.append(padded_batch)
+  #attention_mask.append(batch_attention_mask)
+#  print(batch_attention_mask,'\n',len(batch_attention_mask))
+#  print(padded_batch,'\n',len(padded_batch))
+#  input_ids.append(torch.tensor(padded_batch))
+#  attention_mask.append(torch.tensor(batch_attention_mask))
+
 
 #print(padded)
 #print(attention_mask)
+
+
+# PADDING AND ATTENTION MASK WITHOUT SMART BATCHING
 
 #max_len = 0
 #for sentence in tokenized_dataset.values:
@@ -94,14 +111,13 @@ for batch in random_batches_list:
 #print(padded_tok_dataset)
 #print(numpy.array(padded_tok_dataset).shape)
 
-# ATTENTION MASK
-
 #attention_mask = numpy.where(padded_tok_dataset != 0, 1, 0)
 #print(attention_mask.shape)
 
 #input_ids = torch.tensor(padded_tok_dataset)  
+input_ids = torch.tensor(input_ids)  
 #print(input_ids)
-#attention_mask = torch.tensor(attention_mask)
+attention_mask = torch.tensor(attention_mask)
 #print(attention_mask)
 
 # TRAINING PART THAT MUST BE UNDERSTOOD AND CORRECTED