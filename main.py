efdbde8148bfe49f556e8584119ff531d2399e63
gicraveiro <gmcraveiro@hotmail.com>
2021-08-18 14:42:28 +0200

    deleted comments of wrong approaches and commented pad and attention mask

--- main.py
+++ main.py
@@ -18,19 +18,12 @@ model = model_class.from_pretrained(pretrained_weights)
 dataframe = pandas.read_csv("data/laptop14_train.txt", delimiter='####', header=None, names=['text','labels'],engine='python')
 
 #dataframe tokenized dataset
-print(dataframe['text'].str.len().sort_values())
-print(dataframe['text'].str.len().sort_values().index)
+
 new_index_list = dataframe['text'].str.len().sort_values().index
 dataframe = dataframe.reindex(new_index_list) # sorted dataframe
-print(dataframe)
-print(dataframe['text'].str.len().sort_values())
 dataframe = dataframe.reset_index(drop=True)
-print(dataframe)
 tokenized_dataset = dataframe['text'].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))#, max_length=100, truncation=True, padding=False''' )
 
-print(tokenized_dataset[0])
-print(tokenized_dataset[3044])
-#print(tokenized_dataset['text'].str.len().sort_values())
 #list format tokenized dataset
 #tokenized_dataset = []
 #print(.values.len().sort_values())
@@ -42,30 +35,22 @@ print(tokenized_dataset[3044])
 #print(tokenized_dataset)
 #print(numpy.array(tokenized_dataset).shape)
 
-max_len = 0
-for sentence in tokenized_dataset.values:
-  if (len(sentence) > max_len):
-    max_len = len(sentence)
-print(max_len)
+#max_len = 0
+#for sentence in tokenized_dataset.values:
+#  if (len(sentence) > max_len):
+#    max_len = len(sentence)
+#print(max_len)
 
-#unsorted_lengths = [len(x) for x in tokenized_dataset].sort_values()
-#print(type(tokenized_dataset))
-#print(tokenized_dataset.)
-#tokenized_dataset.reindex(tokenized_dataset.name.len().sort_values().index)
-#tokenized_dataset.sort_values(key=len(x))
-#sorted_lengths = [len(x) for x in tokenized_dataset]
-#print(sorted_lengths)
-
-padded_tok_dataset = numpy.array([i + [0]*(max_len-len(i)) for i in tokenized_dataset.values]) # TO DO: UNDERSTAND
+#padded_tok_dataset = numpy.array([i + [0]*(max_len-len(i)) for i in tokenized_dataset.values]) # TO DO: UNDERSTAND
 #print(padded_tok_dataset)
-print(numpy.array(padded_tok_dataset).shape)
+#print(numpy.array(padded_tok_dataset).shape)
 
-attention_mask = numpy.where(padded_tok_dataset != 0, 1, 0)
+#attention_mask = numpy.where(padded_tok_dataset != 0, 1, 0)
 #print(attention_mask.shape)
 
-input_ids = torch.tensor(padded_tok_dataset)  
+#input_ids = torch.tensor(padded_tok_dataset)  
 #print(input_ids)
-attention_mask = torch.tensor(attention_mask)
+#attention_mask = torch.tensor(attention_mask)
 #print(attention_mask)
 #with torch.no_grad():
 #  last_hidden_states = model(input_ids, attention_mask=attention_mask)
