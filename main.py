5cef703acb967f9dcecac261d07f8173bdb8bdac
gicraveiro <gmcraveiro@hotmail.com>
2021-08-18 18:16:19 +0200

    moved tokenization, padding, attention mask and smart batching to a single reusable function

--- main.py
+++ main.py
@@ -5,7 +5,57 @@ import pandas
 import numpy
 import random
 
-#code extracted from tutorial at http://jalammar.github.io/a-visual-guide-to-using-bert-for-the-first-time/
+
+
+def restart_sampling():
+  # TOKENIZATION
+  tokenized_dataset = dataframe['text'].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))#, max_length=100, truncation=True, padding=False''' )
+
+  # RANDOM BATCH REORDERING
+
+  batch_size = 8
+  dynamic_dataframe = tokenized_dataset.copy(deep=True) #copy of the dataframe to delete it parts
+  random_batches_list = []
+
+  while len(dynamic_dataframe) != 0:
+    random_index = random.randint(0, len(dynamic_dataframe))
+    if (random_index + batch_size >= len(dynamic_dataframe)):
+      random_index = random_index - (random_index + batch_size - len(dynamic_dataframe) +1)
+      if( random_index < 0):
+        random_index = 0
+        batch_size = len(dynamic_dataframe)
+
+    batch = dynamic_dataframe[random_index:(random_index+batch_size)]
+    random_batches_list.append(batch)
+
+    dynamic_dataframe.drop(dynamic_dataframe.index[random_index:random_index+batch_size], inplace=True)
+
+  # PADDING AND ATTENTION MASK WITH SMART BATCHING
+
+  attention_mask = []
+  input_ids = []
+
+  for batch in random_batches_list:
+    max_len = 0
+    for sentence in batch:
+      padded_batch = []
+      batch_attention_mask = []
+
+      if (len(sentence) > max_len):
+        max_len = len(sentence)
+    
+    for sentence in batch:
+      num_zeros = max_len - len(sentence)
+      sentence_attention_mask = (len(sentence)*[1] + num_zeros*[0])
+      sentence = sentence + [0] * num_zeros
+      padded_batch.append(sentence)
+      batch_attention_mask.append(sentence_attention_mask)
+
+    input_ids.append(torch.tensor(padded_batch))
+    attention_mask.append(torch.tensor(batch_attention_mask))
+
+  return(input_ids,attention_mask)
+
 model_class, tokenizer_class, pretrained_weights = (transformers.DistilBertModel, transformers.DistilBertTokenizer, 'distilbert-base-uncased')
 #model_class, tokenizer_class, pretrained_weights = (transformers.BertModel, transformers.BertTokenizer, 'bert-base-uncased')
 
@@ -22,52 +72,9 @@ new_index_list = dataframe['text'].str.len().sort_values().index
 dataframe = dataframe.reindex(new_index_list) # sorted dataframe by length of the sentence
 dataframe = dataframe.reset_index(drop=True)
 
-# TOKENIZATION
-tokenized_dataset = dataframe['text'].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))#, max_length=100, truncation=True, padding=False''' )
-
-# RANDOM BATCH REORDERING
-
-batch_size = 8
-dynamic_dataframe = tokenized_dataset.copy(deep=True) #copy of the dataframe to delete it parts
-random_batches_list = []
-
-while len(dynamic_dataframe) != 0:
-  random_index = random.randint(0, len(dynamic_dataframe))
-  if (random_index + batch_size >= len(dynamic_dataframe)):
-    random_index = random_index - (random_index + batch_size - len(dynamic_dataframe) +1)
-    if( random_index < 0):
-      random_index = 0
-      batch_size = len(dynamic_dataframe)
-
-  batch = dynamic_dataframe[random_index:(random_index+batch_size)]
-  random_batches_list.append(batch)
-
-  dynamic_dataframe.drop(dynamic_dataframe.index[random_index:random_index+batch_size], inplace=True)
-
-# PADDING AND ATTENTION MASK WITH SMART BATCHING
-
-attention_mask = []
-input_ids = []
-
-for batch in random_batches_list:
-  max_len = 0
-  for sentence in batch:
-    padded_batch = []
-    batch_attention_mask = []
-
-    if (len(sentence) > max_len):
-      max_len = len(sentence)
-  
-  for sentence in batch:
-    num_zeros = max_len - len(sentence)
-    sentence_attention_mask = (len(sentence)*[1] + num_zeros*[0])
-    sentence = sentence + [0] * num_zeros
-    padded_batch.append(sentence)
-    batch_attention_mask.append(sentence_attention_mask)
-
-  input_ids.append(torch.tensor(padded_batch))
-  attention_mask.append(torch.tensor(batch_attention_mask))
-
+input_ids, attention_mask = restart_sampling()
+#print(input_ids)
+#print(attention_mask)
 
 # TRAINING PART THAT MUST BE UNDERSTOOD AND CORRECTED
 
@@ -87,4 +94,12 @@ Implementation of the architecture proposed by:
   booktitle={Proceedings of ACL},
   year={2019}
 }
+
+Smart batching reference:
+
+http://mccormickml.com/2020/07/29/smart-batching-tutorial/#uniform-length-batching
+
+First steps reference:
+
+#code extracted from tutorial at http://jalammar.github.io/a-visual-guide-to-using-bert-for-the-first-time/
 ''' 
