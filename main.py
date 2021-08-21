73e7e1a9450169ca1a10caef40d7b9dab6f1772c
gicraveiro <gmcraveiro@hotmail.com>
2021-08-21 23:47:42 +0200

    removed reused paper code file and cleaned some more

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-#import bert.tokenization as tokenizer
 import torch
 import transformers # pytorch transformers
 import pandas
 import math
 import random
 import time
-from transformers import AutoConfig, AdamW, get_linear_schedule_with_warmup#BertConfig
-#from transformers.utils.dummy_pt_objects import LongformerForQuestionAnswering
+from transformers import AutoConfig, AdamW, get_linear_schedule_with_warmup
 import numpy
 
 def restart_sampling(batch_size, input_file):
@@ -127,7 +125,7 @@ def restart_sampling(batch_size, input_file):
 
 initial_time = time.time()
 
-config = AutoConfig.from_pretrained(pretrained_model_name_or_path='distilbert-base-uncased')#,num_labels=2)
+config = AutoConfig.from_pretrained(pretrained_model_name_or_path='distilbert-base-uncased')
 #config = BertConfig(vocab_size=30522)
 #config = BertConfig.from_json_file("bert/bert_config.json") # include bert directory ONLY in local repository
 
@@ -137,18 +135,12 @@ qa_model_class, tokenizer_class, pretrained_weights = (transformers.DistilBertFo
 tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
 model = qa_model_class.from_pretrained(pretrained_weights) 
 
-
-
-# TRAINING CONFIGURATIONS
-
-epochs_qnt = 3#3 # TO DO: CHANGE TO 3
-batch_size = 8
 with open('data/laptop14_train.txt') as file:
   train_dataset_len = sum(1 for line in file)
-#with open('data/laptop14_test.txt') as file:
-#  test_dataset_len = sum(1 for line in file)
-#print(train_dataset_len)
-#print( test_dataset_len)
+# TRAINING CONFIGURATIONS
+
+epochs_qnt = 3
+batch_size = 8
 training_steps = epochs_qnt * math.ceil(train_dataset_len/batch_size)
 
 # Setting optimizer
@@ -176,10 +168,6 @@ for epoch in range(0,epochs_qnt):
     # logits are the predicted outputs by the model before activation
     outputs = model(input_ids=input_ids,  attention_mask=input_mask, start_positions=input_start, end_positions=input_end)
     loss = outputs.loss
-    #start_logits = outputs.start_logits
-    #end_logits = outputs.end_logits
-    #print("Loss", loss)
-    #print(start_logits, end_logits)
     
     loss.backward() # backward propagate
     train_loss += loss.item()
@@ -199,11 +187,10 @@ print("Training time in minutes:", training_time/60)
 
 #EVALUATION
 
-  # PREPARE DATASET ON TEST
-  # need to send the right parameters!
+# PREPARE DATASET ON TEST
 input_ids, attention_mask, start_positions, end_positions = restart_sampling(batch_size, "data/laptop14_test.txt")
 
-model.eval() #set to evaluation mode
+model.eval() # set to evaluation mode
 
 predicted_starts, predicted_ends, real_starts, real_ends = [], [], [], []
 count = 0
@@ -213,12 +200,10 @@ for batch_index,(input_ids, input_mask, input_start, input_end) in enumerate(zip
 
   with torch.no_grad():
     outputs = model(input_ids, attention_mask=input_mask) 
-    
-  #logits = outputs[0]
+
   start_logits = outputs.start_logits
   end_logits = outputs.end_logits
-  #print(start_logits)
-  #print(end_logits)
+
   for logit in start_logits:
     start_logits = logit.numpy()
     end_logits = logit.numpy()
@@ -230,34 +215,20 @@ for batch_index,(input_ids, input_mask, input_start, input_end) in enumerate(zip
     predicted_starts.append(pred_start[0][0])
     predicted_ends.append(pred_end[0][0])
 
-  real_count += len(input_start)
-  #pred_count += len(predicted_starts)
   real_starts.append(input_start)
   real_ends.append(input_end)
 
 eval_time = time.time() - training_time - initial_time
 print('Congrats! Evaluation concluded successfully!\n')
 print('Evaluation time in seconds:', eval_time)
-#print(predicted_starts)
-#print(predicted_ends)
-#print(len(predicted_starts))
-#print(len(predicted_ends))
 
 total_real_starts = numpy.concatenate(real_starts, axis=0)
 total_real_ends = numpy.concatenate(real_ends, axis=0)
-#total_real_starts = total_real_starts[total_real_starts != -1]
-#total_real_ends = total_real_ends[total_real_ends != -1]
 total_real_starts[total_real_starts == -1] = 0
 total_real_ends[total_real_ends == -1] = 0
 total_real_starts = list(total_real_starts)
 total_real_ends = list(total_real_ends)
 
-
-#print(total_real_starts, len(total_real_starts))
-#print(total_real_ends, len(total_real_ends))
-#print(len(predicted_starts))
-#print(len(predicted_ends))
-#print("count", count, real_count, pred_count)
 true_positives = 0
 for index, (pred_start, real_start, pred_end, real_end) in enumerate(zip(predicted_starts,total_real_starts, predicted_ends, total_real_ends)):
   if(predicted_starts[index] == total_real_starts[index] and predicted_ends[index] == total_real_ends[index]):
@@ -274,21 +245,7 @@ print("DistilBERT used instead of BERT")
 print("Number of hidden layers: 4")
 print("Hidden size: 312")
 print("Intermediate size: 1200")
-# ADD REPORTS OF SIZE AND TIME !! -- EFFICIENCY
-
-#total_predicted_starts = numpy.concatenate(predicted_starts, axis=0)
-#total_predicted_ends = numpy.concatenate(predicted_ends, axis=0)
-
-#predicted_start = numpy.argmax(predicted_starts, axis=1).flatten()
-#predicted_end = numpy.argmax(predicted_ends, axis=1).flatten()
-
-#accuracy_starts = (predicted_starts == total_real_starts).mean()
-#accuracy_ends = (predicted_ends == total_real_ends).mean()
-#true_positives = 
-
-#print('Accuracy Start:', accuracy_starts)
-#print('Accuracy Ends', accuracy_ends)
-
+# TO DO: report in a more organized way the EFFICIENCY based on size and time
 
 '''
 Acknowledgments
