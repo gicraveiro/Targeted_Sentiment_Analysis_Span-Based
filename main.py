tried several approaches to solving nan loss problem, likely exploding gradients that I do not know how to fix, changing approach to a new branch

main.py
@@ -5,9 +5,10 @@ import pandas
 import numpy
 import math
 import random
+import os
 from transformers import AutoConfig, AdamW, get_linear_schedule_with_warmup#BertConfig
 from transformers.utils.dummy_pt_objects import LongformerForQuestionAnswering
-from reused import BertConfig, BertForSpanAspectExtraction
+from reused import BertConfig, BertForSpanAspectExtraction, BERTAdam, evaluate
 #from reused import run_train_epoch, read_eval_data, read_train_data, prepare_optimizer
 
 def restart_sampling(batch_size):
@@ -126,16 +127,39 @@ def restart_sampling(batch_size):
 
   return(input_ids,segment_ids,attention_mask, start_positions, end_positions)
 
+def custom_optimizer(model, num_train_steps):
+  param_optimizer = list(model.named_parameters())
+  #print(param_optimizer)
+  no_decay = ['bias', 'LayerNorm']
+  optimizer_grouped_parameters = [
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}]
+  optimizer = BERTAdam(optimizer_grouped_parameters,
+                        lr=2e-5,
+                        warmup=0.1,
+                        t_total=num_train_steps)
+  return optimizer, param_optimizer
+
+#os.makedirs('output')
+save_path = os.path.join('output', 'checkpoint.pth.tar')
+log_path = os.path.join('output', 'performance.txt')
+network_path = os.path.join('output', 'network.txt')
+parameter_path = os.path.join('output', 'parameter.txt')
+
 #config = AutoConfig.from_pretrained(pretrained_model_name_or_path='distilbert-base-uncased')#,num_labels=2)
+#bert_config = BertConfig.from_json_file(args.bert_config_file)
+#config = BertConfig(vocab_size=30522)
+config = BertConfig.from_json_file("bert/bert_config.json") # include bert directory ONLY in local repository
 
 qa_model_class, tokenizer_class, pretrained_weights = (transformers.DistilBertForQuestionAnswering, transformers.DistilBertTokenizer, 'distilbert-base-uncased-distilled-squad') # for QA 'distilbert-base-uncased-distilled-squad' 'distilbert-base-uncased' 'pt-tinybert-msmarco'
+#model_class, tokenizer_class, pretrained_weights = (transformers.BertModel, transformers.BertTokenizer, 'bert-base-uncased')
+
 
 # Load pretrained model/tokenizer
 tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
-model = qa_model_class.from_pretrained(pretrained_weights) 
+#model = qa_model_class.from_pretrained(pretrained_weights) 
+model = BertForSpanAspectExtraction(config)
 
-#bert_config = BertConfig.from_json_file(args.bert_config_file)
-config = BertConfig(vocab_size=30522)
 
 # Creates a table separating sentences from associated token tags
 dataframe = pandas.read_csv("data/laptop14_train.txt", delimiter='####', header=None, names=['text','labels'],engine='python')
@@ -162,7 +186,25 @@ dataframe = dataframe.reset_index(drop=True)
 
 # REUSED FROM PAPER 
 
-bert_config = BertConfig.from_json_file("bert/bert_config.json") # include bert directory ONLY in local repository
+
+#checkpoint = torch.load(save_path)
+#model.load_state_dict(checkpoint['model'])
+#step = checkpoint['step']
+#print("Loading model from finetuned checkpoint: '{}' (step {})"
+#            .format(save_path, step))
+
+f = open(network_path, "w")
+for n, param in model.named_parameters():
+    print("name: {}, size: {}, dtype: {}, requires_grad: {}"
+          .format(n, param.size(), param.dtype, param.requires_grad), file=f)
+total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+total_params = sum(p.numel() for p in model.parameters())
+print("Total trainable parameters: {}".format(total_trainable_params), file=f)
+print("Total parameters: {}".format(total_params), file=f)
+f.close()
+eval_examples, eval_features, eval_dataloader = None, None, None
+#train dataloader, num trainsteps none
+
 
 # tokenization as included in the paper code...
 #tokenizer = tokenizer.FullTokenizer(vocab_file="bert/vocab_file.txt", do_lower_case=True)
@@ -171,28 +213,37 @@ bert_config = BertConfig.from_json_file("bert/bert_config.json") # include bert 
 #model = bert_load_state_dict(model, torch.load("bert/pytorch_model.bin", map_location='cpu')) # TO DO: EXTRACT FUNCTION FROM PAPER CODE
 #print("Loading model from pretrained checkpoint: {}".format("bert/pytorch_model.bin"))
 
+epochs_qnt = 1 # TO DO: CHANGE TO 3
+batch_size = 8
+training_steps = epochs_qnt * math.ceil(len(tokenized_dataset)/batch_size)
+ # This is the learning rate the paper used # args.adam_epsilon  - default is 1e-8.
+#optimizer = AdamW(model.parameters(),lr = 2e-5,eps = 1e-8  )
+optimizer, param_optimizer = custom_optimizer(model, training_steps)
+
 #device = torch.device('cuda') #device = "cpu"
 device = "cpu"
 model.to(device)
 
-optimizer = AdamW(model.parameters(),
-                  lr = 2e-5, # This is the value the paper used
-                  eps = 1e-8 # args.adam_epsilon  - default is 1e-8.
-                )
-epochs_qnt = 1 # TO DO: CHANGE TO 3
-batch_size = 8
+#checkpoint = torch.load(save_path)
+#optimizer.load_state_dict(checkpoint['optimizer'])
+#step = checkpoint['step']
+#print("Loading optimizer from finetuned checkpoint: '{}' (step {})".format(save_path, step))
+global_step = 0 #step
 
-training_steps = epochs_qnt * math.ceil(len(tokenized_dataset)/batch_size)
+best_f1 = 0
+start_save_steps = int(training_steps * 0.5)
+save_checkpoints_steps = int(training_steps / (5 * epochs_qnt))
 
+model.train() # only sets the training mode
 #num warmup steps is default value in glue.py
-scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps = 0,num_training_steps = training_steps)
+#scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps = 0,num_training_steps = training_steps)
 for epoch in range(0,epochs_qnt):
 
   input_ids, segment_ids, attention_mask, start_positions, end_positions = restart_sampling(batch_size=batch_size)
   #print(type(input_ids), type(segment_ids), type(attention_mask), type(start_positions), type(end_positions))
 
-  train_loss = 0
-  model.train() # only sets the training mode
+  train_loss = 0.0
+  count = 0
 
   for batch_index,(ids, seg, mask, start, end) in enumerate(zip(input_ids, segment_ids, attention_mask, start_positions, end_positions)):
     #print(batch)
@@ -202,7 +253,7 @@ for epoch in range(0,epochs_qnt):
     #input_start = start_positions[index].to(device)
     #input_end = end_positions[index].to(device)
 
-    model.zero_grad() #clear previous gradients
+    #model.zero_grad() #clear previous gradients
 
     #print(input_ids.shape,type(input_ids))
     # TRAINING STEP
@@ -210,11 +261,12 @@ for epoch in range(0,epochs_qnt):
     # logits are the predicted outputs by the model before activation
 
     # TO DO: find out what are segment_ids and create them!
-    
-    model = BertForSpanAspectExtraction(bert_config)
+    #print(type(train_loss))
+    #model = BertForSpanAspectExtraction(bert_config)
     #loss, start_logits, end_logits = model(input_ids=input_ids,  attention_mask=input_mask, start_positions=input_start, end_positions=input_end)
     #print(type(ids), type(seg), type(mask), type(start), type(end))
     loss = model(ids, seg, mask, start, end)
+    #start_logits, end_logits = model(ids, seg, mask)
     #score_start, score_end = model(input_ids, input_mask)
     #tokens = inputIds[torch.argmax(scores_start): torch.argmax(score_end) + 1]
     #answerTokens = distilBertTokenizer.convert_ids_to_tokens(tokens, skip_special_tokens=True)
@@ -228,24 +280,55 @@ for epoch in range(0,epochs_qnt):
     #start_logits = start_logits.squeeze(-1)
     #end_logits = end_logits.squeeze(-1)
 
-    
+    #print(loss)
     loss.backward() # backward propagate
     train_loss += loss.item()
+    if numpy.isnan(train_loss) == False:
+      print(type(train_loss), train_loss)
 
     # Clip the norm of the gradients to 1.0.
     # This is to help prevent the "exploding gradients" problem.
-    torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    #torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
 
     optimizer.step() #update parameters 
-    scheduler.step() #update learning rate
+    model.zero_grad()
+    global_step += 1
+    count += 1
+'''
+    if global_step % save_checkpoints_steps == 0 and count != 0:
+      print("step: {}, loss: {:.4f}".format(global_step, train_loss / count))
+
+    if global_step % save_checkpoints_steps == 0 and global_step > start_save_steps and count != 0:  # eval & save model
+      print("***** Running evaluation *****")
+      model.eval()
+      metrics = evaluate(model, device, eval_examples, eval_features, eval_dataloader)
+      f = open("output/performance.txt", "a")
+      print("step: {}, loss: {:.4f}, P: {:.4f}, R: {:.4f}, F1: {:.4f} (common: {}, retrieved: {}, relevant: {})"
+            .format(global_step, train_loss / count, metrics['p'], metrics['r'],
+                    metrics['f1'], metrics['common'], metrics['retrieved'], metrics['relevant']), file=f)
+      print(" ", file=f)
+      f.close()
+      train_loss, count = 0.0, 0
+      model.train()
+      if metrics['f1'] > best_f1:
+          best_f1 = metrics['f1']
+          torch.save({
+              'model': model.state_dict(),
+              'optimizer': optimizer.state_dict(),
+              'step': global_step
+          }, "output/test")
+
+
+    #scheduler.step() #update learning rate
     #print(input_ids, input_mask, input_start, input_end)
     #print(batch.to(device))
+    '''
 
   #print("SCORE START", score_start)
   #print("SCORE END", score_end)
-  print("Total loss", train_loss)
-  print("Average loss", train_loss/math.ceil(len(tokenized_dataset)/batch_size))
-  print("Congrats!Training concluded successfully!")
+print("Total loss", train_loss)
+print("Average loss", train_loss/math.ceil(len(tokenized_dataset)/batch_size))
+print("Congrats!Training concluded successfully!")
 '''
 #EVALUATION
 