b607aa809b33b6e0de781f687cde8d04ebd8764b
gicraveiro <gmcraveiro@hotmail.com>
2021-08-21 19:19:41 +0200

    some code cleaning

--- main.py
+++ main.py
@@ -2,14 +2,11 @@
 import torch
 import transformers # pytorch transformers
 import pandas
-import numpy
 import math
 import random
-import os
 from transformers import AutoConfig, AdamW, get_linear_schedule_with_warmup#BertConfig
-from transformers.utils.dummy_pt_objects import LongformerForQuestionAnswering
-#from reused import BertConfig, BertForSpanAspectExtraction, BERTAdam, evaluate
-#from reused import run_train_epoch, read_eval_data, read_train_data, prepare_optimizer
+#from transformers.utils.dummy_pt_objects import LongformerForQuestionAnswering
+#import numpy
 
 def restart_sampling(batch_size):
   # TOKENIZATION
@@ -48,7 +45,7 @@ def restart_sampling(batch_size):
   input_ids = []
   start_positions = []
   end_positions = []
-  segment_ids = []
+  #segment_ids = []
 
   for batch,labels in zip(random_batches_list,random_labels_list):
     max_len = 0
@@ -57,7 +54,7 @@ def restart_sampling(batch_size):
       batch_attention_mask = []
       batch_start_positions = []
       batch_end_positions = []
-      batch_segment_ids = []
+      #batch_segment_ids = []
 
       if (len(sentence) > max_len):
         max_len = len(sentence)
@@ -102,7 +99,7 @@ def restart_sampling(batch_size):
         sentence_start_positions = [-1]
         sentence_end_positions = [-1]
       sentence = sentence + [0] * num_zeros
-      batch_segment_ids = max_len * [0]
+      #batch_segment_ids = max_len * [0]
       padded_batch.append(sentence)
       batch_attention_mask.append(sentence_attention_mask)
       batch_start_positions += sentence_start_positions
@@ -112,24 +109,19 @@ def restart_sampling(batch_size):
     attention_mask.append(torch.tensor(batch_attention_mask, dtype=torch.long))
     start_positions.append(torch.tensor(batch_start_positions, dtype=torch.long))
     end_positions.append(torch.tensor(batch_end_positions, dtype=torch.long))
-    segment_ids.append(torch.tensor(batch_segment_ids, dtype=torch.long))
+    #segment_ids.append(torch.tensor(batch_segment_ids, dtype=torch.long))
 
-  return(input_ids,segment_ids,attention_mask, start_positions, end_positions)
-
+  return(input_ids,attention_mask, start_positions, end_positions)
 
 config = AutoConfig.from_pretrained(pretrained_model_name_or_path='distilbert-base-uncased')#,num_labels=2)
 #config = BertConfig(vocab_size=30522)
 #config = BertConfig.from_json_file("bert/bert_config.json") # include bert directory ONLY in local repository
 
-qa_model_class, tokenizer_class, pretrained_weights = (transformers.DistilBertForQuestionAnswering, transformers.DistilBertTokenizer, 'distilbert-base-uncased') # for QA 'distilbert-base-uncased-distilled-squad' 'distilbert-base-uncased' 'pt-tinybert-msmarco'
-#model_class, tokenizer_class, pretrained_weights = (transformers.BertModel, transformers.BertTokenizer, 'bert-base-uncased')
-
+qa_model_class, tokenizer_class, pretrained_weights = (transformers.DistilBertForQuestionAnswering, transformers.DistilBertTokenizer, 'distilbert-base-uncased') 
 
 # Load pretrained model/tokenizer
 tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
 model = qa_model_class.from_pretrained(pretrained_weights) 
-#model = BertForSpanAspectExtraction(config)
-
 
 # Creates a table separating sentences from associated token tags
 dataframe = pandas.read_csv("data/laptop14_train.txt", delimiter='####', header=None, names=['text','labels'],engine='python')
@@ -141,76 +133,44 @@ new_index_list = dataframe['text'].str.len().sort_values().index
 dataframe = dataframe.reindex(new_index_list) # sorted dataframe by length of the sentence
 dataframe = dataframe.reset_index(drop=True)
 
+# TRAINING CONFIGURATIONS
 
-epochs_qnt = 1 # TO DO: CHANGE TO 3
+epochs_qnt = 3 # TO DO: CHANGE TO 3
 batch_size = 8
 training_steps = epochs_qnt * math.ceil(len(tokenized_dataset)/batch_size)
+
+# Setting optimizer
  # This is the learning rate the paper used # args.adam_epsilon  - default is 1e-8.
 optimizer = AdamW(model.parameters(),lr = 2e-5,eps = 1e-8  ) #change learning rate
 
 device = "cpu"
 model.to(device)
 
-best_f1 = 0
-start_save_steps = int(training_steps * 0.5)
-save_checkpoints_steps = int(training_steps / (5 * epochs_qnt))
+# TRAINING STEP
 
 model.train() # only sets the training mode
 #num warmup steps is default value in glue.py
 scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps = 0,num_training_steps = training_steps)
 for epoch in range(0,epochs_qnt):
-
-  input_ids, segment_ids, attention_mask, start_positions, end_positions = restart_sampling(batch_size=batch_size)
-  #print(type(input_ids), type(segment_ids), type(attention_mask), type(start_positions), type(end_positions))
-
+  
   train_loss = 0.0
-  #count = 0
+  input_ids,  attention_mask, start_positions, end_positions = restart_sampling(batch_size=batch_size)
 
-  for batch_index,(input_ids, seg, input_mask, input_start, input_end) in enumerate(zip(input_ids, segment_ids, attention_mask, start_positions, end_positions)):
-    #print(batch)
-
-    #input_ids = ids.to(device) # TO DO: CONFIRM THAT IT IS USEFUL TO MOVE THEM TO CPU, OR DELETE IF ITS NOT DOING ANYTHING
-    #input_mask = mask.to(device)
-    #input_start = start.to(device)
-    #input_end = end.to(device)
+  for batch_index,(input_ids, input_mask, input_start, input_end) in enumerate(zip(input_ids, attention_mask, start_positions, end_positions)):
 
     model.zero_grad() #clear previous gradients
 
-    #print(input_ids.shape,type(input_ids))
-    # TRAINING STEP
     #loss is returned because it is supervised learning based on the labels
     # logits are the predicted outputs by the model before activation
-
-    # TO DO: find out what are segment_ids and create them!
-    #print(type(train_loss))
-    #model = BertForSpanAspectExtraction(bert_config)
-    #print(type(ids), type(seg), type(mask), type(start), type(end))
-    #loss = model(ids, seg, mask, start, end)
     outputs = model(input_ids=input_ids,  attention_mask=input_mask, start_positions=input_start, end_positions=input_end)
-    #loss, start_logits, end_logits
-    #start_logits, end_logits = model(ids, seg, mask)
-    #score_start, score_end = model(input_ids, input_mask)
-    #tokens = inputIds[torch.argmax(scores_start): torch.argmax(score_end) + 1]
-    #answerTokens = distilBertTokenizer.convert_ids_to_tokens(tokens, skip_special_tokens=True)
-    #print(tokenizer.convert_tokens_to_string(answerTokens))
-    # extra layer
-    #all_encoder_layers, _ = self.bert(input_ids, segment_ids, attention_mask)
-    #sequence_output = all_encoder_layers[-1]
-    #self.qa_outputs = nn.Linear(config.hidden_size, 2)
-    #logits = self.qa_outputs(sequence_output)
-    #start_logits, end_logits = logits.split(1, dim=-1)
-    #start_logits = start_logits.squeeze(-1)
-    #end_logits = end_logits.squeeze(-1)
     loss = outputs.loss
-    start_logits = outputs.start_logits
-    end_logits = outputs.end_logits
-    print(loss, start_logits, end_logits)
+    #start_logits = outputs.start_logits
+    #end_logits = outputs.end_logits
+    print("Loss", loss)
+    #print(start_logits, end_logits)
     
-
     loss.backward() # backward propagate
     train_loss += loss.item()
-    if numpy.isnan(train_loss) == False:
-      print(type(train_loss), train_loss)
 
     # Clip the norm of the gradients to 1.0.
     # This is to help prevent the "exploding gradients" problem.
@@ -219,41 +179,7 @@ for epoch in range(0,epochs_qnt):
     optimizer.step() #update parameters 
     scheduler.step()
     #model.zero_grad()
-    #global_step += 1
-    #count += 1
-'''
-    if global_step % save_checkpoints_steps == 0 and count != 0:
-      print("step: {}, loss: {:.4f}".format(global_step, train_loss / count))
 
-    if global_step % save_checkpoints_steps == 0 and global_step > start_save_steps and count != 0:  # eval & save model
-      print("***** Running evaluation *****")
-      model.eval()
-      metrics = evaluate(model, device, eval_examples, eval_features, eval_dataloader)
-      f = open("output/performance.txt", "a")
-      print("step: {}, loss: {:.4f}, P: {:.4f}, R: {:.4f}, F1: {:.4f} (common: {}, retrieved: {}, relevant: {})"
-            .format(global_step, train_loss / count, metrics['p'], metrics['r'],
-                    metrics['f1'], metrics['common'], metrics['retrieved'], metrics['relevant']), file=f)
-      print(" ", file=f)
-      f.close()
-      train_loss, count = 0.0, 0
-      model.train()
-      if metrics['f1'] > best_f1:
-          best_f1 = metrics['f1']
-          torch.save({
-              'model': model.state_dict(),
-              'optimizer': optimizer.state_dict(),
-              'step': global_step
-          }, "output/test")
-
-
-    #scheduler.step() #update learning rate
-    #print(input_ids, input_mask, input_start, input_end)
-    #print(batch.to(device))
-    '''
-
-  #print("SCORE START", score_start)
-  #print("SCORE END", score_end)
-print("Total loss", train_loss)
 print("Average loss", train_loss/math.ceil(len(tokenized_dataset)/batch_size))
 print("Congrats!Training concluded successfully!")
 '''
@@ -265,82 +191,22 @@ print("Congrats!Training concluded successfully!")
 
   model.eval() #set to evaluation mode
 
-  predictions, start_labels, end_labels = [], []
+  predicted_starts, predicted_ends, real_starts, real_ends = [], [], [], []
 
-  for index, batch in enumerate(input_ids):
-
-    input_ids = batch.to(device) # TO DO: CONFIRM THAT IT IS USEFUL TO MOVE THEM TO CPU, OR DELETE IF ITS NOT DOING ANYTHING
-    input_mask = attention_mask[index].to(device)
-    input_start = start_positions[index].to(device)
-    input_end = end_positions[index].to(device)
+  for batch_index,(input_ids, input_mask, input_start, input_end) in enumerate(zip(input_ids, attention_mask, start_positions, end_positions)):
 
     with torch.no_grad():
-      outputs = model(input_ids, token_type_ids=None, attention_mask=input_mask) # check token_type_ids
+      outputs = model(input_ids, attention_mask=input_mask) 
       
-    logits = outputs[0]
+    #logits = outputs[0]
+    start_logits = outputs.start_logits
+    end_logits = outputs.end_logits
 
-    # Move logits and labels to CPU
-    logits = logits.detach().cpu().numpy()
-    start_ids = input_start.to('cpu').numpy()
-    end_ids = input_end.to('cpu').numpy()
-
-    predictions.append(logits)
-    start_labels.append(start_ids)
-    end_labels.append(end_ids)
-
+    predicted_starts.append(start_logits)
+    predicted_ends.append(end_logits)
+    real_starts.append(input_starts)
+    real_ends.append(input_ends)
 '''
-'''
-print("***** Preparing data *****")
-train_dataloader, num_train_steps = None, None
-eval_examples, eval_features, eval_dataloader = None, None, None
-train_batch_size = 4 
-
-# their tokenizer was the FullTokenizer but let's head it with this one for now and see what happens
-print("***** Preparing training *****")
-train_dataloader, num_train_steps = read_train_data(tokenizer)
-
-print("***** Preparing evaluation *****")
-eval_examples, eval_features, eval_dataloader = read_eval_data(tokenizer)
-print("***** Preparing optimizer *****")
-optimizer, param_optimizer = prepare_optimizer(model, num_train_steps)
-
-print("***** Running training *****")
-best_f1 = 0
-save_checkpoints_steps = int(num_train_steps / (5 * 3)) # 3 = num_train_epochs
-start_save_steps = int(num_train_steps * 0.5) # 0.5 = save proportion
-model.train()
-global_step = 0
-'''
-
-#for epoch in range(3):
-#  print("***** Epoch: {} *****".format(epoch+1))
-#  global_step, model, best_f1 = run_train_epoch(global_step, model, param_optimizer, train_dataloader, eval_examples, eval_features, eval_dataloader, optimizer, 0, device, 'out/extract/01/performance.txt', 'out/extract/01/checkpoint.pth.tar', save_checkpoints_steps, start_save_steps, best_f1) #n_gpu = 0
-'''
-print("***** Running prediction *****")
-        if eval_dataloader is None:
-            eval_examples, eval_features, eval_dataloader = read_eval_data(args, tokenizer, logger)
-
-        # restore from best checkpoint
-        if save_path and os.path.isfile(save_path) and args.do_train:
-            checkpoint = torch.load(save_path)
-            model.load_state_dict(checkpoint['model'])
-            step = checkpoint['step']
-            logger.info("Loading model from finetuned checkpoint: '{}' (step {})"
-                        .format(save_path, step))
-
-        model.eval()
-        metrics = evaluate(args, model, device, eval_examples, eval_features, eval_dataloader, logger, write_pred=True)
-        f = open(log_path, "a")
-        print("threshold: {}, step: {}, P: {:.4f}, R: {:.4f}, F1: {:.4f} (common: {}, retrieved: {}, relevant: {})"
-              .format(args.logit_threshold, global_step, metrics['p'], metrics['r'],
-                      metrics['f1'], metrics['common'], metrics['retrieved'], metrics['relevant']), file=f)
-        print(" ", file=f)
-        f.close()
-        '''
-
-
-#print(input_ids)
-#print(attention_mask)
 
 '''
 Acknowledgments
